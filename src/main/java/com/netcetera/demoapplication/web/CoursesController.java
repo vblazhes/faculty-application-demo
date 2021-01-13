6b6355670ce165ca71985f38d69ae9e7fa3d42e4
vblazhes <62813356+vblazhes@users.noreply.github.com>
2021-01-13 13:31:55 +0100

    Basic Courses controller added

---
+++ src/main/java/com/netcetera/demoapplication/web/CoursesController.java
@@ -0,0 +1,33 @@
+package com.netcetera.demoapplication.web;
+
+import com.netcetera.demoapplication.model.Course;
+import com.netcetera.demoapplication.service.CoursesService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/courses")
+public class CoursesController {
+
+    CoursesService coursesService;
+
+    CoursesController(CoursesService coursesService){
+        this.coursesService = coursesService;
+    }
+
+    @GetMapping
+    public List<Course> getAllCourses(){
+        return coursesService.getAllCourses();
+    }
+
+    @GetMapping("/{code}")
+    public Course getCourseByCode(@PathVariable("code") String code){
+        return coursesService.getCourseByCode(code);
+    }
+
+}
