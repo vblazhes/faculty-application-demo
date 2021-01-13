ba2523e3b361d0052e79a2ce389d6499e1042572
vblazhes <62813356+vblazhes@users.noreply.github.com>
2021-01-13 13:25:01 +0100

    Professors Service Implementaion added

---
+++ src/main/java/com/netcetera/demoapplication/service/Impl/ProfessorsServiceImpl.java
@@ -0,0 +1,37 @@
+package com.netcetera.demoapplication.service.Impl;
+
+import com.netcetera.demoapplication.model.Professor;
+import com.netcetera.demoapplication.model.exception.ProfessorNotFoundException;
+import com.netcetera.demoapplication.repository.ProfessorsRepository;
+import com.netcetera.demoapplication.service.ProfessorsService;
+
+import java.util.List;
+
+public class ProfessorsServiceImpl implements ProfessorsService {
+
+    ProfessorsRepository professorsRepository;
+
+    ProfessorsServiceImpl(ProfessorsRepository professorsRepository){
+        this.professorsRepository = professorsRepository;
+    }
+
+    @Override
+    public List<Professor> getAllProfessors() {
+        return professorsRepository.findAll();
+    }
+
+    @Override
+    public Professor getProfessorById(Long id) {
+        return professorsRepository.findById(id).orElseThrow(ProfessorNotFoundException::new);
+    }
+
+    @Override
+    public Professor save(Professor professor) {
+        return professorsRepository.save(professor);
+    }
+
+    @Override
+    public void deleteProfessorById(Long id) {
+        professorsRepository.deleteById(id);
+    }
+}
