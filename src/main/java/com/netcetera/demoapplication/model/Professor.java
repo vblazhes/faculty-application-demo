e1588d2cc97257143a015eddfbcd97fa25766adb
vblazhes <62813356+vblazhes@users.noreply.github.com>
2021-01-14 16:34:05 +0100

    OneToMany bi-directional relationship to Courses added

--- src/main/java/com/netcetera/demoapplication/model/Professor.java
+++ src/main/java/com/netcetera/demoapplication/model/Professor.java
@@ -1,14 +1,14 @@
 package com.netcetera.demoapplication.model;
 
+import com.fasterxml.jackson.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
@@ -26,5 +26,31 @@ public class Professor {
     private String firstName;
 
     private String lastName;
+
+//    /** The way to map unidirectional OneToMany **/
+//    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+//    @JoinColumn(name = "professor_id")
+//    Set<Course> courses;
+
+    /**
+     * The way to map BiDirectional OneToMany
+     **/
+    @OneToMany(
+            mappedBy = "professor",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    @JsonIgnore
+    Set<Course> courses = new HashSet<>();
+
+    public void addCourse(Course course) {
+        courses.add(course);
+        course.setProfessor(this);
+    }
+
+    public void removeCourse(Course course) {
+        courses.remove(course);
+        course.setProfessor(null);
+    }
 
 }
